a20ae3110129933f93bbfe2bbaf501598697805f
Philipp Bobek <philipp.bobek@mailbox.org>
2023-12-05 21:31:09 +0100

    Update Guava to 32.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
 
     implementation("com.google.android.material:material:1.10.0")
 
-    implementation("com.google.guava:guava:31.1-android")
+    implementation("com.google.guava:guava:32.1.3-android")
 
     testImplementation("junit:junit:4.13.2")
 
