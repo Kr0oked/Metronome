7002c462e0959cf215eae5d80b32d2fdfbd2ee80
Philipp Bobek <philipp.bobek@mailbox.org>
2024-12-01 20:45:31 +0100

    Release 1.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.bobek.metronome"
         minSdk = 21
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.6.2"
+        versionCode = 15
+        versionName = "1.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
